ea72bfe9837889aadd09a0a968918570b4ac0607
Steven Davidovitz <steven.davidovitz@dominodatalab.com>
2022-05-12 21:16:22 -0700

    run expose.sh from anywhere

--- scripts/development/remote-context/expose.sh
+++ scripts/development/remote-context/expose.sh
@@ -2,8 +2,11 @@
 #
 set -e
 
-tmpdir=$(mktemp -d -t hephaestus)
-tar -czf "$tmpdir/context.tgz" remote-context/Dockerfile
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd "$SCRIPT_DIR" || exit 1
+
+tmpdir=$(mktemp -d)
+tar -czf "$tmpdir/context.tgz" Dockerfile
 python3 -m http.server --directory "$tmpdir"
 
 trap "{ rm -rf $tmpdir; }" SIGINT SIGTERM ERR EXIT
