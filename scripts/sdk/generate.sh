1157036b121910bdf53ad1d8772856ebad736cb1
Sonny Garcia <sonnygarcia@icloud.com>
2022-07-25 14:19:31 -0600

    use the branch name for snapshots

--- scripts/sdk/generate.sh
+++ scripts/sdk/generate.sh
@@ -66,7 +66,7 @@ generate_kubernetes_swagger () {
 
 GIT_TAG=$(git describe --tags --candidates=0 --abbrev=0 2> /dev/null || echo untagged)
 if [[ $GIT_TAG == "untagged" ]]; then
-  VERSION="$(git rev-parse --abbrev-ref HEAD)-SNAPSHOT"
+  VERSION="${GITHUB_REF_NAME:-0.0.0}-SNAPSHOT"
 else
   VERSION="${GIT_TAG#v}"
 fi
