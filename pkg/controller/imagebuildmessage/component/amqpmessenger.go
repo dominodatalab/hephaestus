c988bd9546b3a6a5d9220a9d24b517259c37a186
ddl-rliu <richard.liu@dominodatalab.com>
2024-01-04 17:13:38 -0500

    Try annotations

--- pkg/controller/imagebuildmessage/component/amqpmessenger.go
+++ pkg/controller/imagebuildmessage/component/amqpmessenger.go
@@ -204,7 +204,7 @@ func (c *AMQPMessengerComponent) Reconcile(ctx *core.Context) (ctrl.Result, erro
 			message.ImageURLs = images
 			message.Annotations["imagebuilder.dominodatalab.com/image-size"] = "123456789"
 			annotations := obj.GetAnnotations()
-			log.V(1).Info("Hello2 annotations", "annotations", annotations)
+			log.Info("Hello2 annotations", "annotations", annotations)
 		case hephv1.PhaseFailed:
 			if ib.Status.Conditions == nil {
 				return ctrl.Result{Requeue: true}, nil
