e12cc4a1085ac00c87831de4ef08d05935d2a040
ddl-rliu <richard.liu@dominodatalab.com>
2024-02-27 12:06:01 -0800

    Update description

--- pkg/api/hephaestus/v1/imagebuild_types.go
+++ pkg/api/hephaestus/v1/imagebuild_types.go
@@ -51,8 +51,8 @@ type ImageBuildStatus struct {
 	BuilderAddr string `json:"builderAddr,omitempty"`
 	// CompressedImageSizeBytes is the total size of all the compressed layers in the image.
 	CompressedImageSizeBytes string `json:"compressedImageSizeBytes,omitempty"`
-	// Map of string keys and values corresponding OCI image config labels
-	// Labels contains arbitrary metadata for the container
+	// Map of string keys and values corresponding OCI image config labels.
+	// Labels contains arbitrary metadata for the container.
 	Labels map[string]string `json:"labels,omitempty"`
 
 	Conditions  []metav1.Condition     `json:"conditions,omitempty"`
