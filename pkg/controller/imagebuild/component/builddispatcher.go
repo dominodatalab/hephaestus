956a26c5ec1bdc8fb0fc25485920fc3355e09672
Steven Davidovitz <steven.davidovitz@dominodatalab.com>
2024-04-11 01:10:13 -0700

    remove secrets

--- pkg/controller/imagebuild/component/builddispatcher.go
+++ pkg/controller/imagebuild/component/builddispatcher.go
@@ -244,7 +244,7 @@ func (c *BuildDispatcherComponent) Reconcile(coreCtx *core.Context) (ctrl.Result
 			log.Info("Build cancelled via resource delete")
 			txn.AddAttribute("cancelled", true)
 
-			//nolint:nilerr // we want reconciliation to pass and end her
+			//nolint:nilerr // we want reconciliation to pass and end here
 			return ctrl.Result{}, nil
 		}
 
