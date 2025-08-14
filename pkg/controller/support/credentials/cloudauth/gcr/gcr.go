b0f0fbb0f84c3c8954681bd018d00089b3f2d9c1
Joaquin Lopez <joaquin.lopez@dominodatalab.com>
2025-08-13 18:39:28 -0700

    Don't use the registry token in gcr

--- pkg/controller/support/credentials/cloudauth/gcr/gcr.go
+++ pkg/controller/support/credentials/cloudauth/gcr/gcr.go
@@ -163,6 +163,6 @@ func (g *gcrProvider) authenticate(
 	return &registry.AuthConfig{
 		Username:      "oauth2accesstoken",
 		Password:      token.AccessToken,
-		RegistryToken: response.Token,
+		RegistryToken: "",
 	}, nil
 }
